3314627900e3e44cd420b24116afe49d97bc97cd
Dominik Barczewski <barczewski@e-spirit.com>
2021-02-17 11:00:40 +0100

    feat(fetchprojectproperties): added fetchProjectProperties method and better development enviroment
    The method "fetchProjectProperties" was implemented, which provides the project setting of a
    respective language. However, this only works from CaaSConnect version 3. Also a dev folder was
    created to give developers a better possibility to develop the FSXA API.

---
+++ dev/README.md
@@ -0,0 +1,15 @@
+# Development
+
+This folder is for enabling better development for the FSXA-API.
+
+The `index.ts` file imports the FSXA-API from the `src` folder and makes all functions available so that you can test them directly during development.
+
+There you can call them and see if the FSXA-API behaves as desired during development
+
+
+## Getting Started 
+
+Copy the `.env.template` file, rename it to `.env` and write there all needed variables.
+These will be loaded during execution.
+
+To start the test environment, you can enter the `npm run dev` in the terminal.
---
+++ dev/index.ts
@@ -0,0 +1,47 @@
+import {inspect} from "util";
+import dotenv from "dotenv"
+import express from "express";
+import {FSXAApi, FSXAContentMode} from "../src"
+const expressIntegration = require('../dist/lib/integrations/express').default
+require('cross-fetch/polyfill')
+
+dotenv.config({path: './dev/.env'})
+const app = express()
+
+const {API_MODE, API_API_KEY, API_NAVIGATION_SERVICE, API_CAAS, API_PROJECT_ID, API_TENANT_ID} = process.env
+
+const remoteApi = new FSXAApi(
+  FSXAContentMode.PREVIEW,
+  {
+    mode: API_MODE as any,
+    config: {
+      apiKey: API_API_KEY!,
+      navigationService: API_NAVIGATION_SERVICE!,
+      caas: API_CAAS!,
+      projectId: API_PROJECT_ID!,
+      tenantId: API_TENANT_ID!
+    }
+  },
+  3
+)
+app.use('/api', expressIntegration({ api: remoteApi }))
+
+app.listen(3001, async () => {
+  console.log('Listening at http://localhost:3001')
+
+  const proxyApi = new FSXAApi(
+    FSXAContentMode.PREVIEW,
+    {
+      mode: 'proxy',
+      baseUrl: 'http://localhost:3001/api'
+    },
+    3
+  )
+  try {
+    const response = await proxyApi.fetchProjectProperties('de')
+    console.log(inspect(response, false, null, true))
+ } catch (err) {
+    console.log('ERROR', err)
+  }
+
+})
