8e04683039722392aeb4dbd3dbd9666d234baf77
Lukas Schneider <schneider@e-spirit.com>
2022-02-04 14:11:28 +0100

    refactor(fsxaremoteapi fetchnavigation): remove authData from logging

--- src/modules/FSXARemoteApi.ts
+++ src/modules/FSXARemoteApi.ts
@@ -253,7 +253,7 @@ export class FSXARemoteApi implements FSXAApi {
     this._logger.info('fetchNavigation', 'start', {
       locale,
       initialPath,
-      authData,
+      authDataPassed: !!authData,
     })
     let encodedInitialPath = undefined
     if (initialPath) {
