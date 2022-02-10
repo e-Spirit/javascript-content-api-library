dd8b58546895d5ac36f7b6b26766fb00e493a9c7
Philipp Runge <p.runge@progani.com>
2022-02-10 12:16:31 +0100

    fix(api-singleton): fix reinitialization of FSXAApiSingleton

--- src/modules/FSXAApiSingleton.ts
+++ src/modules/FSXAApiSingleton.ts
@@ -6,14 +6,18 @@ export class FSXAApiSingleton {
   private static _logger: Logger
 
   public static init(api: FSXAApi, logLevel = LogLevel.ERROR) {
-    this._logger = new Logger(logLevel, 'FSXAApiSingleton')
-    this._logger.info('FSXA-Api initialized with api:', api)
-    if (this._api) {
-      this._logger.warn(
-        'The FSXA-Api has already been initialized - the api will NOT be initialized again! You can ignore this message in a development scenario.'
-      )
+    if (!this._api) {
+      this._logger = new Logger(logLevel, 'FSXAApiSingleton')
+      this._api = api
+      this._logger.debug('FSXA-Api initialized with api:', api)
+
+      return this._api
     }
-    this._api = api
+
+    this._logger.warn(
+      'The FSXA-Api has already been initialized - the api will NOT be initialized again! You can ignore this message in a development scenario.'
+    )
+
     return this._api
   }
 
@@ -21,6 +25,7 @@ export class FSXAApiSingleton {
     if (!this._api) {
       throw new Error('The FSXA-Api needs to be initialized. Call .init() first.')
     }
+
     return this._api
   }
 }
