a8a6d49791188fb9f8c42d37aed1b6da49e3f04f
Dean Gite <dgite@ritech.co>
2023-04-06 15:37:46 +0200

    add tests with different formats

--- src/modules/XMLParser.spec.ts
+++ src/modules/XMLParser.spec.ts
@@ -88,12 +88,14 @@ describe('XMLParser', () => {
         expect(result).toEqual(expectedValue)
       }
     )
-    it('should quotes the slashes', async () => {
-      let oldFormat = "<script type=\\\"application\/json\\\">{\\\"spacing\\\":{\\\"fsType\\\":\\\"CMS_INPUT_COMBOBOX\\\",\\\"name\\\":\\\"spacing\\\",\\\"value\\\":{\\\"fsType\\\":\\\"Option\\\",\\\"label\\\":\\\"14px\\\\\/1.0rem\\\",\\\"identifier\\\":\\\"10\\\"}}}<\/script>"
-      const xmlNewFormat =
-        '<script type=\\\"application/json\\\">{\\\"spacing\\\":{\\\"fsType\\\":\\\"CMS_INPUT_COMBOBOX\\\",\\\"name\\\":\\\"spacing\\\",\\\"value\\\":{\\\"fsType\\\":\\\"Option\\\",\\\"label\\\":\\\"14px/1.0rem\\\",\\\"identifier\\\":\\\"10\\\"}}}</script>'
-      const res = await xmlParser.parse(oldFormat)
-      console.log(res)
+    it('CORE-11147 should not have impact in output format', async () => {
+      let oldFormat = `<div data-fs-style=\"format.standard\"><div data-fs-type=\"link.internal_link_2\"><script type=\"application/json\">{\"lt_richtext\":{\"fsType\":\"CMS_INPUT_DOM\",\"name\":\"lt_richtext\",\"value\":\"<p><div data-fs-type=\\\"link.internal_link_2\\\"><script type=\\\"application\/json\\\">{\\\"lt_richtext\\\":{\\\"fsType\\\":\\\"CMS_INPUT_DOM\\\",\\\"name\\\":\\\"lt_richtext\\\",\\\"value\\\":\\\"<p>nested link lvl2<\\\\\/p>\\\"},\\\"lt_some_image\\\":{\\\"fsType\\\":\\\"FS_REFERENCE\\\",\\\"name\\\":\\\"lt_some_image\\\",\\\"value\\\":null},\\\"lt_text\\\":{\\\"fsType\\\":\\\"CMS_INPUT_TEXT\\\",\\\"name\\\":\\\"lt_text\\\",\\\"value\\\":\\\"nested element lvl2\\\"}}<\/script><a>nested element lvl2<\/a><\/div><\/p>\"},\"lt_some_image\":{\"fsType\":\"FS_REFERENCE\",\"name\":\"lt_some_image\",\"value\":null},\"lt_text\":{\"fsType\":\"CMS_INPUT_TEXT\",\"name\":\"lt_text\",\"value\":\"nested element lvl1\"}}</script><a>nested element lvl1</a></div></div>`
+      let newFormat = `<div data-fs-style=\"format.standard\"><div data-fs-type=\"link.internal_link_2\"><script type=\"application/json\">{\"lt_richtext\":{\"fsType\":\"CMS_INPUT_DOM\",\"name\":\"lt_richtext\",\"value\":\"<p><div data-fs-type=\\\"link.internal_link_2\\\"><script type=\\\"application/json\\\">{\\\"lt_richtext\\\":{\\\"fsType\\\":\\\"CMS_INPUT_DOM\\\",\\\"name\\\":\\\"lt_richtext\\\",\\\"value\\\":\\\"<p>nested link lvl2</p>\\\"},\\\"lt_some_image\\\":{\\\"fsType\\\":\\\"FS_REFERENCE\\\",\\\"name\\\":\\\"lt_some_image\\\",\\\"value\\\":null},\\\"lt_text\\\":{\\\"fsType\\\":\\\"CMS_INPUT_TEXT\\\",\\\"name\\\":\\\"lt_text\\\",\\\"value\\\":\\\"nested element lvl2\\\"}}</script><a>nested element lvl2</a></div></p>\"},\"lt_some_image\":{\"fsType\":\"FS_REFERENCE\",\"name\":\"lt_some_image\",\"value\":null},\"lt_text\":{\"fsType\":\"CMS_INPUT_TEXT\",\"name\":\"lt_text\",\"value\":\"nested element lvl1\"}}</script><a>nested element lvl1</a></div></div>`
+      // sanitize and parse rich texts
+      const parsedOldFormat = await xmlParser.parse(oldFormat)
+      const parsedNewFormat = await xmlParser.parse(newFormat)
+      // expect both outputs to be equal
+      expect(JSON.stringify(parsedNewFormat)).toEqual(JSON.stringify(parsedOldFormat))
     })
   })
 
