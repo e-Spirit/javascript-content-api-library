c7e74bb6a83b4640af7a9defeb94cc48ae22ed45
Dean Gite <deangite@gmail.com>
2023-04-14 16:46:42 +0200

    fix(xmlparser): fix parsing nested elements in richtext (#159)
    * fix(xmlparser): fix parsing nested elements
    * refactor(utils.ts): improve code quality of richtext helper methods

--- src/modules/XMLParser.spec.ts
+++ src/modules/XMLParser.spec.ts
@@ -22,7 +22,11 @@ describe('XMLParser', () => {
   it('should parse &apos correct', async () => {
     const xml = "<div>'</div>"
     const expectedValue = [
-      { content: [{ content: "'", data: {}, type: 'text' }], data: {}, type: 'block' },
+      {
+        content: [{ content: "'", data: {}, type: 'text' }],
+        data: {},
+        type: 'block',
+      },
     ]
     const result = await xmlParser.parse(xml)
 
@@ -32,7 +36,11 @@ describe('XMLParser', () => {
   it('should parse &nbsp; correctly', async () => {
     const xml = '<div>&nbsp;</div>'
     const expectedValue = [
-      { data: {}, content: [{ type: 'text', content: '\xa0', data: {} }], type: 'block' },
+      {
+        data: {},
+        content: [{ type: 'text', content: '\xa0', data: {} }],
+        type: 'block',
+      },
     ]
     const result = await xmlParser.parse(xml)
     expect(result).toEqual(expectedValue)
@@ -80,6 +88,17 @@ describe('XMLParser', () => {
         expect(result).toEqual(expectedValue)
       }
     )
+    it('CORE-11147 should not have impact in output format', async () => {
+      let oldFormat = `<div data-fs-style=\"format.standard\"><div data-fs-type=\"link.internal_link_2\"><script type=\"application/json\">{\"lt_richtext\":{\"fsType\":\"CMS_INPUT_DOM\",\"name\":\"lt_richtext\",\"value\":\"<p><div data-fs-type=\\\"link.internal_link_2\\\"><script type=\\\"application\/json\\\">{\\\"lt_richtext\\\":{\\\"fsType\\\":\\\"CMS_INPUT_DOM\\\",\\\"name\\\":\\\"lt_richtext\\\",\\\"value\\\":\\\"<p>nested link lvl2<\\\\\/p>\\\"},\\\"lt_some_image\\\":{\\\"fsType\\\":\\\"FS_REFERENCE\\\",\\\"name\\\":\\\"lt_some_image\\\",\\\"value\\\":null},\\\"lt_text\\\":{\\\"fsType\\\":\\\"CMS_INPUT_TEXT\\\",\\\"name\\\":\\\"lt_text\\\",\\\"value\\\":\\\"nested element lvl2\\\"}}<\/script><a>nested element lvl2<\/a><\/div><\/p>\"},\"lt_some_image\":{\"fsType\":\"FS_REFERENCE\",\"name\":\"lt_some_image\",\"value\":null},\"lt_text\":{\"fsType\":\"CMS_INPUT_TEXT\",\"name\":\"lt_text\",\"value\":\"nested element lvl1\"}}</script><a>nested element lvl1</a></div></div>`
+      let newFormat = `<div data-fs-style=\"format.standard\"><div data-fs-type=\"link.internal_link_2\"><script type=\"application/json\">{\"lt_richtext\":{\"fsType\":\"CMS_INPUT_DOM\",\"name\":\"lt_richtext\",\"value\":\"<p><div data-fs-type=\\\"link.internal_link_2\\\"><script type=\\\"application/json\\\">{\\\"lt_richtext\\\":{\\\"fsType\\\":\\\"CMS_INPUT_DOM\\\",\\\"name\\\":\\\"lt_richtext\\\",\\\"value\\\":\\\"<p>nested link lvl2</p>\\\"},\\\"lt_some_image\\\":{\\\"fsType\\\":\\\"FS_REFERENCE\\\",\\\"name\\\":\\\"lt_some_image\\\",\\\"value\\\":null},\\\"lt_text\\\":{\\\"fsType\\\":\\\"CMS_INPUT_TEXT\\\",\\\"name\\\":\\\"lt_text\\\",\\\"value\\\":\\\"nested element lvl2\\\"}}</script><a>nested element lvl2</a></div></p>\"},\"lt_some_image\":{\"fsType\":\"FS_REFERENCE\",\"name\":\"lt_some_image\",\"value\":null},\"lt_text\":{\"fsType\":\"CMS_INPUT_TEXT\",\"name\":\"lt_text\",\"value\":\"nested element lvl1\"}}</script><a>nested element lvl1</a></div></div>`
+      // sanitize and parse rich texts
+      const parsedOldFormat = await xmlParser.parse(oldFormat)
+      const parsedNewFormat = await xmlParser.parse(newFormat)
+      // expect both outputs to be equal
+      expect(JSON.stringify(parsedNewFormat)).toEqual(
+        JSON.stringify(parsedOldFormat)
+      )
+    })
   })
 
   describe('Link Element', () => {
@@ -97,7 +116,9 @@ describe('XMLParser', () => {
       const dataIdentifier = 'data-test-id'
       const testValue = '2'
       const xml = `<link ${dataIdentifier}="${testValue}"></link>`
-      const expectedValue = [{ content: [], data: { [dataIdentifier]: testValue }, type: 'link' }]
+      const expectedValue = [
+        { content: [], data: { [dataIdentifier]: testValue }, type: 'link' },
+      ]
       const result = await xmlParser.parse(xml)
 
       expect(result).toEqual(expectedValue)
