621af2c9dbf640b05db6d27ae41c34ed14fa7044
henczi <henczi@e-spirit.com>
2023-04-14 14:52:17 +0200

    refactor(utils.ts): improve code quality of richtext helper methods

--- src/utils.ts
+++ src/utils.ts
@@ -43,26 +43,17 @@ export const isValidRegex = (str: string) => {
   }
 }
 
-// restructure the link structure into one single link element  (hint: *? matches non-eager)
-// we need to find only the top-level link templates and replace them
-// as we have link templates inside link templates as well as multiple top-level link templates in one element
-// the tricky part is that the closing tag for a nested link template does not "end" the wrapping link template
-export function replaceUnnestedLinkTemplatesWithOneLinkElement(
-  input: string,
-  ENTITIES: Map<string, string>
-): string {
+function findOpeningLinkTags(
+  input: string
+): { index: number; openingTag: string }[] {
   const openingTagRegex =
     /<div data-fs-type=\\*?"link\.(.*?)\\*?">\\*?<script type=\\*?"application\/json\\*?">/
-  const closingTagRegex = /<\/script>\s*<a>(.*?)<\/a>\s*<\/div>/
-  let result = input
-
-  // Find all opening link tags in the input
+  let result: { index: number; openingTag: string }[] = []
   let i = 0
-  const openingTags: { index: number; openingTag: string }[] = []
   while (i < input.length) {
     const openingTagMatch = openingTagRegex.exec(input.substring(i))
     if (openingTagMatch) {
-      openingTags.push({
+      result.push({
         index: openingTagMatch.index + i,
         openingTag: openingTagMatch[0],
       })
@@ -71,74 +62,91 @@ export function replaceUnnestedLinkTemplatesWithOneLinkElement(
       break
     }
   }
+  return result
+}
 
+// restructure the link structure into one single link element  (hint: *? matches non-eager)
+// we need to find only the top-level link templates and replace them
+// as there can be link templates inside link templates as well as multiple top-level link templates in one element
+// the tricky part is that the closing tag for a nested link template does not "end" the wrapping link template
+export function replaceUnnestedLinkTemplatesWithOneLinkElement(
+  input: string,
+  ENTITIES: Map<string, string>
+): string {
+  const openingTags = findOpeningLinkTags(input)
   // replace link templates if there are any
-  if (openingTags.length > 0) {
-    let openingTagsIndex = 0
-    let currentIndex = openingTags[openingTagsIndex].index
-    const stack = []
-    stack.push(openingTags[openingTagsIndex])
+  if (openingTags.length <= 0) {
+    return input
+  }
 
-    while (stack.length > 0 && currentIndex < input.length) {
-      // Find the next closing tag between opening index and next opening tag (or input end if there are no more opening tags)
-      const closingTagMatch = closingTagRegex.exec(
-        input.substring(
-          currentIndex,
-          openingTags[openingTagsIndex + 1]?.index || input.length
+  const openingTagRegex =
+    /<div data-fs-type=\\*?"link\.(.*?)\\*?">\\*?<script type=\\*?"application\/json\\*?">/
+  const closingTagRegex = /<\/script>\s*<a>(.*?)<\/a>\s*<\/div>/
+  let result = input
+  let openingTagsIndex = 0
+  let currentIndex = openingTags[openingTagsIndex].index
+  const stack = []
+  stack.push(openingTags[openingTagsIndex])
+
+  while (stack.length > 0 && currentIndex < input.length) {
+    // Find the next closing tag between opening index and next opening tag (or input end if there are no more opening tags)
+
+    const nextOpeningTag =
+      openingTags[openingTagsIndex + 1]?.index || input.length
+    const closingTags = closingTagRegex.exec(
+      input.substring(currentIndex, nextOpeningTag)
+    )
+    if (closingTags) {
+      // if we are at outermost level (not nested into another link template) replace the element in result with link element
+      if (stack.length === 1) {
+        const realClosingTagIndex = currentIndex + closingTags.index
+        const opening = stack[0]
+        const elementToReplace = input.substring(
+          opening.index,
+          realClosingTagIndex + closingTags[0].length
         )
-      )
-      if (closingTagMatch) {
-        // if we are at outermost level (not nested into another link template) replace the element in result with link element
-        if (stack.length === 1) {
-          const realClosingTagIndex = currentIndex + closingTagMatch.index
-          const opening = stack[0]
-          const elementToReplace = input.substring(
-            opening.index,
-            realClosingTagIndex + closingTagMatch[0].length
+        const data = input
+          .substring(
+            opening.index + opening.openingTag.length,
+            realClosingTagIndex
           )
-          const data = input
-            .substring(
-              opening.index + opening.openingTag.length,
-              realClosingTagIndex
-            )
-            // Since java 4 backslashes are considered as one backslash, we need to replace them with two backslashes
-            .replace(/\\\\/g, '\\')
-            .replace(
-              /(["&<\\])/g,
-              (...args: any[]) => ENTITIES.get(args[1]) || args[1]
-            )
-            .replace(/&#34;/g, '\\&quot;')
-          const type = opening.openingTag.replace(
-            openingTagRegex,
-            (...args: any[]) => args[1]
+          // Since java 4 backslashes are considered as one backslash, we need to replace them with two backslashes
+          .replace(/\\\\/g, '\\')
+          .replace(
+            /(["&<\\])/g,
+            (...args: any[]) => ENTITIES.get(args[1]) || args[1]
           )
-          const content = closingTagMatch[0].replace(
-            closingTagRegex,
-            (...args: any[]) => args[1]
-          )
-          result = result.replace(
-            elementToReplace,
-            `<link type="${type}" data="${data}">${content}</link>`
-          )
-        }
-        // remove the corresponding opening tag from stack as it is closed now
-        stack.pop()
-        // set current index to end of closing tag, this is where we want to continue our search for closing tags
-        currentIndex += closingTagMatch.index + closingTagMatch[0].length
-      } else {
-        // no closing tag was found, set the current index to the next opening tag or to the end of the input if there are no more opening tags
-        currentIndex = openingTags[openingTagsIndex + 1]?.index || input.length
+          .replace(/&#34;/g, '\\&quot;')
+        const type = opening.openingTag.replace(
+          openingTagRegex,
+          (...args: any[]) => args[1]
+        )
+        const content = closingTags[0].replace(
+          closingTagRegex,
+          (...args: any[]) => args[1]
+        )
+        result = result.replace(
+          elementToReplace,
+          `<link type="${type}" data="${data}">${content}</link>`
+        )
       }
-      // add the next opening tag to the stack if there are any
-      // but only if there are no open tags left, or there are open tags but the next opening tag needs to be checked as there was no closing tag found in the currently checked substring
-      if (
-        openingTagsIndex < openingTags.length - 1 &&
-        (stack.length == 0 ||
-          openingTags[openingTagsIndex + 1].index <= currentIndex)
-      ) {
-        openingTagsIndex += 1
-        stack.push(openingTags[openingTagsIndex])
-      }
+      // remove the corresponding opening tag from stack as it is closed now
+      stack.pop()
+      // set current index to end of closing tag, this is where we want to continue our search for closing tags
+      currentIndex += closingTags.index + closingTags[0].length
+    } else {
+      // no closing tag was found, set the current index to the next opening tag or to the end of the input if there are no more opening tags
+      currentIndex = openingTags[openingTagsIndex + 1]?.index || input.length
+    }
+    // add the next opening tag to the stack if there are any
+    // but only if there are no open tags left, or there are open tags but the next opening tag needs to be checked as there was no closing tag found in the currently checked substring
+    if (
+      openingTagsIndex < openingTags.length - 1 &&
+      (stack.length == 0 ||
+        openingTags[openingTagsIndex + 1].index <= currentIndex)
+    ) {
+      openingTagsIndex += 1
+      stack.push(openingTags[openingTagsIndex])
     }
   }
   return result
