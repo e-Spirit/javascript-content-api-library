7241cd5a030ec7220790945e772beafad7b18461
Dean Gite <deangite@gmail.com>
2023-06-16 17:01:51 +0200

    feat(fsxaapisingleton): update singleton to instatiate many instances (#166)

--- src/modules/FSXAApiSingleton.ts
+++ src/modules/FSXAApiSingleton.ts
@@ -11,26 +11,25 @@ export class FSXAApiSingleton {
   private static _logger: Logger
 
   public static init(api: FSXAApi, options: FSXAApiSingletonInitOptions = {}) {
-    if (!this._api) {
-      this._logger = new Logger(options.logLevel || LogLevel.ERROR, 'FSXAApiSingleton')
-      this._api = api
-      this._logger.debug('FSXA-Api initialized with api:', api)
+    this._logger = new Logger(
+      options.logLevel || LogLevel.ERROR,
+      'FSXAApiSingleton'
+    )
+    this._api = api
+    this._logger.debug('FSXA-Api initialized with api:', api)
 
-      if (typeof options.enableEventStream !== 'undefined') {
-        this._api.enableEventStream(options.enableEventStream)
-      }
-
-      return this._api
+    if (typeof options.enableEventStream !== 'undefined') {
+      this._api.enableEventStream(options.enableEventStream)
     }
-
-    this._logger.info('The FSXA-Api has already been initialized - the api will NOT be initialized again!')
 
     return this._api
   }
 
   public static get instance() {
     if (!this._api) {
-      throw new Error('The FSXA-Api needs to be initialized. Call .init() first.')
+      throw new Error(
+        'The FSXA-Api needs to be initialized. Call .init() first.'
+      )
     }
 
     return this._api
