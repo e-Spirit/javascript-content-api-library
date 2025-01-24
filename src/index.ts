306257b889157b2c507ecebd85c5facf603e2720
Alec Winter <alec.winter@monday-consulting.com>
2025-01-24 17:25:24 +0700

    fix(#214): export EvaluationQueryOperatorEnum

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export {
   ArrayQueryOperatorEnum,
   CaaSMapperErrors,
   ComparisonQueryOperatorEnum,
+  EvaluationQueryOperatorEnum,
   LogicalQueryOperatorEnum,
   QueryBuilderErrors,
   LogLevel,
